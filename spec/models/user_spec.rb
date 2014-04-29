0daadf8c622f33ce0483a56363d9c200560ee367
Neil <neil-lu@graduate.uwa.edu.au>
2014-04-29 20:32:58 +0800

    add validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,15 @@
 require 'spec_helper'
 
 describe User do
-  it 'is invalid without an email'
-  it 'is invalid without an password'
-  it 'is invalid if password is not comfirmed'
+  it 'is invalid without an email' do
+    FactoryGirl.build(:user, email: nil).should_not be_valid
+  end
+
+  it 'is invalid without an password' do
+    FactoryGirl.build(:user, password: nil).should_not be_valid
+  end
+
+  it 'is invalid if password is not comfirmed' do
+    FactoryGirl.build(:user, password_confirmation: '1').should_not be_valid
+  end
 end
