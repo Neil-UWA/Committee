d761dac0a2216696b7adc141c1f55af20b79f988
Neil <neil-lu@graduate.uwa.edu.au>
2014-04-29 21:30:44 +0800

    use devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 CommitteeApp::Application.routes.draw do
-  devise_for :users
   root "welcome#index"
+
+  devise_for :users
+
   resources :sessions, only: [:new, :create]
-  resources :users, only: [:new, :create, :show]
   resources :problems, only: [:new, :create, :index]
+
 end
