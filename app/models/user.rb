36607c1316369c7a3b3e42cd445e16bf01f7c2de
Neil <neil-lu@graduate.uwa.edu.au>
2014-04-29 20:36:20 +0800

    add uniqueness to email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :problems
 
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
   validates :password, confirmation: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,4 +12,9 @@ describe User do
   it 'is invalid if password is not comfirmed' do
     FactoryGirl.build(:user, password_confirmation: '1').should_not be_valid
   end
+
+  it 'is invalid to create an user with same email' do
+    FactoryGirl.create(:user)
+    FactoryGirl.build(:user).should_not be_valid
+  end
 end
